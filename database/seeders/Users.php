8f736be8fa959da7bc2f1846ed8d5978c746278a
DESKTOP-Q32OT96\user <mahesa.23@students.amikom.ac.id>
2023-03-07 07:13:43 +0700

    no message

---
+++ database/seeders/Users.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class Users extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'bas',
+            'email' => 'baskorogg@gmail.com',
+            'password' => Hash::make('admin123ok')
+        ]);
+    }
+}
