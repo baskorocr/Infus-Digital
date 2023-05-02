45d368cae4e0befd59f656d3132138fd1cb5181f
Krisna Aditya Pratama <krisnaadityapratama@students.amikom.ac.id>
2023-05-02 14:35:58 +0700

    Commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,10 +36,7 @@
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('home') }}">
-        <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
-        </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Super Admin</div>
     </a>
 
     <!-- Divider -->
@@ -69,11 +66,14 @@
         </a>
         <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Setting</h6>
+                <h6 class="collapse-header">Manajement Alat</h6>
 
-                <a class="collapse-item" href="{{route('device')}}">daftar alat</a>
-                <a class="collapse-item" href="{{route('adddevice')}}">tambah alat</a>
-                <a class="collapse-item" href="{{route('regist')}}">tambah admin</a>
+                <a class="collapse-item" href="{{route('device')}}">Daftar alat</a>
+                <a class="collapse-item" href="{{route('adddevice')}}">Tambah Alat</a>
+            </div>
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">User</h6>
+                <a class="collapse-item" href="{{route('regist')}}">Tambah Admin</a>
             </div>
         </div>
     </li>
